214c52f34b6fd1f104d7c1f72599c2a6370356e3
EtienneDosSantos <130935112+EtienneDosSantos@users.noreply.github.com>
2024-02-17 17:31:14 +0100

    Update run.py

--- run.py
+++ run.py
@@ -24,6 +24,9 @@ def generate_images(prompt, height, width, negative_prompt, guidance_scale, num_
     output_directory = "./Output"
     os.makedirs(output_directory, exist_ok=True)
     output_images = []
+    calculated_steps_prior = int(num_inference_steps * 2 / 3)
+    calculated_steps_decoder = int(num_inference_steps * 1 / 3)
+    generator = torch.Generator("cuda")
 
     with torch.cuda.amp.autocast(dtype=dtype):
         prior_output = prior(
@@ -33,7 +36,7 @@ def generate_images(prompt, height, width, negative_prompt, guidance_scale, num_
             negative_prompt=negative_prompt,
             guidance_scale=float(guidance_scale),
             num_inference_steps=int(calculated_steps_prior),
-            num_images_per_prompt=int(num_images_per_prompt)
+            num_images_per_prompt=int(num_images_per_prompt),
             generator=generator,
         )
         decoder_output = decoder(
@@ -61,8 +64,10 @@ iface = gr.Interface(
         gr.Slider(minimum=512, maximum=2048, step=1, value=1024, label="Image Height"),
         gr.Slider(minimum=512, maximum=2048, step=1, value=1024, label="Image Width"),
         gr.Textbox(label="Negative Prompt", value=""),
-        gr.Slider(minimum=1, maximum=20, step=0.5, value=7.5, label="Guidance Scale"),
-        gr.Number(label="Number of Images per Prompt", value=2)
+        gr.Slider(minimum=1, maximum=20, step=0.5, value=4.0, label="Guidance Scale"), # CFG 4.0 is recommended for Stable Cascade
+        gr.Slider(minimum=1, maximum=150, step=1, value=30, label="Steps"), # Is `step=1` necessary?
+        gr.Number(label="Number of Images per Prompt", value=2),
+        gr.Number(label="Seed", value=-1) # Need to add `value=-1` means random seed.
     ],
     outputs=gr.Gallery(label="Generated Images"),
     title="Image Generator",
