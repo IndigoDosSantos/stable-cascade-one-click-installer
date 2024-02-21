3fe964e6707fe903d77c2d4c9ee033094f6c69ff
Badass Baby Duck <31091783+Acephalia@users.noreply.github.com>
2024-02-22 10:58:20 +1100

    Update run.py

--- run.py
+++ run.py
@@ -20,26 +20,37 @@ dtype = torch.bfloat16
 prior = StableCascadePriorPipeline.from_pretrained("stabilityai/stable-cascade-prior", torch_dtype=dtype).to(device)
 decoder = StableCascadeDecoderPipeline.from_pretrained("stabilityai/stable-cascade", torch_dtype=dtype).to(device)
 
-def generate_images(prompt, height, width, negative_prompt, guidance_scale, num_images_per_prompt):
+def generate_images(prompt, height, width, negative_prompt, guidance_scale, num_inference_steps, num_images_per_prompt, generator):
     output_directory = "./Output"
     os.makedirs(output_directory, exist_ok=True)
     output_images = []
+    calculated_steps_prior = int(num_inference_steps * 2 / 3)
+    calculated_steps_decoder = int(num_inference_steps * 1 / 3)
 
     with torch.cuda.amp.autocast(dtype=dtype):
+        if generator == -1: # Check if user wants a random seed
+            generator = torch.Generator(device).manual_seed(torch.seed())
+        else:
+            generator = torch.Generator(device).manual_seed(generator)
+            
         prior_output = prior(
             prompt=prompt,
             height=int(height),
             width=int(width),
             negative_prompt=negative_prompt,
             guidance_scale=float(guidance_scale),
+            num_inference_steps=int(calculated_steps_prior),
             num_images_per_prompt=int(num_images_per_prompt),
+            generator=generator,
         )
         decoder_output = decoder(
             image_embeddings=prior_output.image_embeddings,
             prompt=prompt,
             negative_prompt=negative_prompt,
             guidance_scale=0.0,
+            num_inference_steps=calculated_steps_decoder,
             output_type="pil",
+            generator=generator,
         ).images
 
     for image in decoder_output:
@@ -57,8 +68,10 @@ iface = gr.Interface(
         gr.Slider(minimum=512, maximum=2048, step=1, value=1024, label="Image Height"),
         gr.Slider(minimum=512, maximum=2048, step=1, value=1024, label="Image Width"),
         gr.Textbox(label="Negative Prompt", value=""),
-        gr.Slider(minimum=1, maximum=20, step=0.5, value=7.5, label="Guidance Scale"),
-        gr.Number(label="Number of Images per Prompt", value=2)
+        gr.Slider(minimum=1, maximum=20, step=0.5, value=4.0, label="Guidance Scale"), # CFG 4.0 is recommended for Stable Cascade
+        gr.Slider(minimum=1, maximum=150, step=1, value=30, label="Steps"), # Is `step=1` necessary?
+        gr.Number(label="Number of Images per Prompt", value=2),
+        gr.Number(label="Seed", value=-1) # Need to add `value=-1` means random seed.
     ],
     outputs=gr.Gallery(label="Generated Images"),
     title="Image Generator",
