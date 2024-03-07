6c6eb83cbddabaafbc14e9002d41a2280dadc2f8
EtienneDosSantos <130935112+EtienneDosSantos@users.noreply.github.com>
2024-03-07 14:10:46 +0100

    Create sort.py
    Use to sort JSON data

---
+++ prompt_configurator/sort.py
@@ -0,0 +1,10 @@
+list_of_terms = [
+    # Add terms here you want sorted.
+]
+
+# Sort the list alphabetically (optional)
+list_of_terms.sort()
+
+# Print the sorted list with quotes and comma
+for list_term in list_of_terms:
+    print(f'"{list_term}", ')
