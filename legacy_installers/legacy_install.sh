e2f61d2524d41ced8ee729c5a8effe2015c8c4e6
EtienneDosSantos <130935112+EtienneDosSantos@users.noreply.github.com>
2024-03-16 18:50:29 +0100

    Update legacy_install.sh

--- legacy_installers/legacy_install.sh
+++ legacy_installers/legacy_install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
-cd "$(dirname "$0")"
+
+# Move to the directory one level up from the script's location
+cd "$(dirname "$0")"/..
+
 # Check for Python and exit if not found
 if ! [ -x "$(command -v python)" ]; then
   echo 'Error: python is not installed.' >&2
   exit 1
 fi
-# Create a virtual environment
+
+# Create a virtual environment in the current directory (now one level up)
 if [ ! -d "venv" ]; then
   python -m venv venv
 fi
@@ -14,7 +18,7 @@ fi
 # Upgrade pip before `pip install`
 ./venv/bin/python -m pip install --upgrade pip
 
-# Install other requirements
+# Install other requirements, assuming requirements.txt is also one level up
 ./venv/bin/pip install -r requirements.txt
 
-echo "Installation completed. Execute `run.sh` script to start generating!"
+echo "Installation completed. Execute 'run.sh' script to start generating!"
